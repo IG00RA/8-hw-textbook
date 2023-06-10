ec9ce5cf62cede659f3a656e5efac54a389aef2d
Igor Gromadskiy <gromadskiyigor@gmail.com>
2023-06-10 13:37:50 +0300

    add src

---
+++ src/redux/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit';
+//=============== Before ========================
+// import { tasksReducer, filtersReducer } from "./reducer";
+//=============== After ========================
+import { tasksReducer } from './tasksSlice';
+import { filtersReducer } from './filtersSlice';
+export const store = configureStore({
+  reducer: {
+    tasks: tasksReducer,
+    filters: filtersReducer,
+  },
+});
